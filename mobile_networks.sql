67b0a6074f255f1680b94b6899e821ba73ce3742
Lokeshchandraroy <163843856+Lokeshchandraroy@users.noreply.github.com>
2024-04-11 10:30:10 +0530

    Add files via upload

---
+++ mobile_networks.sql
@@ -0,0 +1,8 @@
+#STORED DATA IN MOBILE_NETWORKS DATABASE
+
+#Table are two tables in it
+# 1. mobile_data
+# 2. fixed_broadband
+
+SELECT * FROM mobile_networks.mobile_data;
+select * from mobile_networks.fixed_broadband;
